39cd0d97f50defc6141f47f213f44b552e0715df
rasulvh <rasulvh@code.edu.az>
2023-02-04 21:25:18 +0400

    task was added

---
+++ script.js
@@ -0,0 +1,95 @@
+// Task 1
+
+// let num = prompt("Eded daxil edin: ")
+
+// if(!parseInt(num)){
+//     alert("Duzgun daxil edin")
+// }
+// else{
+//     if(num%21==0){
+//         alert("Eded 3e ve 7e tam bolunur")
+//     }
+//     else{
+//         alert("Eded 3e ve 7e tam bolunmur")
+//     }
+// }
+
+
+
+
+
+//Task 2
+
+// let num1 = prompt("1ci ededi daxil edin: ")
+
+// let num2 = prompt("2ci ededi daxil edin: ")
+
+// if(!parseInt(num1) || !parseInt(num2)){
+//     alert("Duzgun daxil edin")
+// }
+
+// else {
+//     if (num1 % 2 == 0 && num2 % 2 == 0) {
+//         alert(Number(num1) + Number(num2))
+//     }
+//     else {
+//         alert("Eded tekdir")
+//     }
+// }
+
+//Task 3
+
+// let n = prompt("N ededi: ")
+// let m = prompt("M ededi: ")
+// let count = 0
+
+// if(!parseInt(n) || !parseInt(m)){
+//     alert("Duzgun daxil edin")
+// }
+// else{
+//     for(let i = n; i<m; i++){
+//         if(i%2==1){
+//             count++
+//         }
+//     }
+    
+//     alert(`${n} ve ${m} ededleri arasindaki tek ededlerin sayi: ${count}`)
+// }
+
+//Task 4
+
+// let n = prompt("N ededi: ")
+// let m = prompt("M ededi: ")
+// let sum = 0
+
+// if(!parseInt(n) || !parseInt(m)){
+//     alert("Duzgun daxil edin")
+// }
+// else{
+//     for(let i = n; i<m; i++){
+//         if(i%2==1){
+//             sum= Number(sum) + Number(i)
+//         }
+//     }
+    
+//     alert(`${n} ve ${m} ededleri arasindaki tek ededlerin cemi: ${sum}`)
+// }
+
+//Task 5
+
+// let n = prompt("N ededi: ")
+// let m = prompt("M ededi: ")
+// let numbers = 0;
+
+// if(!parseInt(n) || !parseInt(m)){
+//     alert("Duzgun daxil edin")
+// }
+// else{
+//     for(let i = n; i<m; i++){
+//         if(i%2==1){
+//             numbers+=(" " + i + " ")
+//         }
+//     }
+    
+//     alert(`${n} ve ${m} ededleri arasindaki tek ededler: ${numbers}`)
+// }
